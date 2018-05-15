4d7927daf4d65e8ae480eabe2138f628647cebfd
Filipe Nogueira Jordão <filipejrd@hotmail.com>
2018-05-15 19:49:16 -0300

    Fixing encoding

--- ryu/scripts.sh
+++ ryu/scripts.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-curl -X POST -d ‘{“address”:”1.1.1.1/30”}’ http://localhost:8080/router/0000000000000001
-curl -X POST -d ‘{“address”:”12.12.12.1/30”}’ http://localhost:8080/router/0000000000000001
-curl -X POST -d ‘{“address”:”2.2.2.1/30”}’ http://localhost:8080/router/0000000000000002
-curl -X POST -d ‘{“address”:”12.12.12.2/30”}’ http://localhost:8080/router/0000000000000002 
-curl -X POST -d ‘{“address”:”3.3.3.1/30”}’ http://localhost:8080/router/0000000000000003
-curl -X POST -d ‘{“address”:”12.12.12.3/30”}’ http://localhost:8080/router/0000000000000003 
-curl -X POST -d ‘{“gateway”:”12.12.12.2”}’ http://localhost:8080/router/0000000000000001
-curl -X POST -d ‘{“gateway”:”12.12.12.3”}’ http://localhost:8080/router/0000000000000002
-curl -X POST -d ‘{“gateway”:”12.12.12.1”}’ http://localhost:8080/router/0000000000000003
+curl -X POST -d '{"address":"1.1.1.1/30"}' http://localhost:8080/router/0000000000000001
+curl -X POST -d '{"address":"12.12.12.1/30"}' http://localhost:8080/router/0000000000000001
+curl -X POST -d '{"address":"2.2.2.1/30"}' http://localhost:8080/router/0000000000000002
+curl -X POST -d '{"address":"12.12.12.2/30"}' http://localhost:8080/router/0000000000000002 
+curl -X POST -d '{"address":"3.3.3.1/30"}' http://localhost:8080/router/0000000000000003
+curl -X POST -d '{"address":"12.12.12.3/30"}' http://localhost:8080/router/0000000000000003 
+curl -X POST -d '{"gateway":"12.12.12.2"}' http://localhost:8080/router/0000000000000001
+curl -X POST -d '{"gateway":"12.12.12.3"}' http://localhost:8080/router/0000000000000002
+curl -X POST -d '{"gateway":"12.12.12.1"}' http://localhost:8080/router/0000000000000003
