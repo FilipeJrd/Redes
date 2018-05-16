aeac2054a1094c3bc5781942b9a89a4f8883aa63
Filipe Nogueira Jordão <filipejrd@hotmail.com>
2018-05-15 21:38:48 -0300

    add firewall rules

--- ryu/scripts.sh
+++ ryu/scripts.sh
@@ -8,3 +8,30 @@ curl -X POST -d '{"address":"12.12.12.3/30"}' http://localhost:8080/router/00000
 curl -X POST -d '{"gateway":"12.12.12.2"}' http://localhost:8080/router/0000000000000001
 curl -X POST -d '{"gateway":"12.12.12.3"}' http://localhost:8080/router/0000000000000002
 curl -X POST -d '{"gateway":"12.12.12.1"}' http://localhost:8080/router/0000000000000003
+
+curl -X PUT http://localhost:8080/firewall/module/enable/0000000000000001
+curl -X POST -d '{"nw_src": "1.1.1.3/30", "nw_dst": "2.2.2.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001
+curl -X POST -d '{"nw_src": "1.1.1.3/30", "nw_dst": "2.2.2.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000002
+curl -X POST -d '{"nw_src": "1.1.1.3/30", "nw_dst": "2.2.2.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000003
+
+curl -X POST -d '{"nw_src": "2.2.2.3/30", "nw_dst": "1.1.1.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001
+curl -X POST -d '{"nw_src": "2.2.2.3/30", "nw_dst": "1.1.1.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000002
+curl -X POST -d '{"nw_src": "2.2.2.3/30", "nw_dst": "1.1.1.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000003
+
+curl -X POST -d '{"nw_src": "1.1.1.3/30", "nw_dst": "3.3.3.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001
+curl -X POST -d '{"nw_src": "1.1.1.3/30", "nw_dst": "3.3.3.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000002
+curl -X POST -d '{"nw_src": "1.1.1.3/30", "nw_dst": "3.3.3.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000003
+
+
+curl -X POST -d '{"nw_src": "3.3.3.3/30", "nw_dst": "1.1.1.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001
+curl -X POST -d '{"nw_src": "3.3.3.3/30", "nw_dst": "1.1.1.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000002
+curl -X POST -d '{"nw_src": "3.3.3.3/30", "nw_dst": "1.1.1.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000003
+
+
+curl -X POST -d '{"nw_src": "2.2.2.3/30", "nw_dst": "3.3.3.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001
+curl -X POST -d '{"nw_src": "2.2.2.3/30", "nw_dst": "3.3.3.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000002
+curl -X POST -d '{"nw_src": "2.2.2.3/30", "nw_dst": "3.3.3.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000003
+
+curl -X POST -d '{"nw_src": "3.3.3.3/30", "nw_dst": "2.2.2.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000001
+curl -X POST -d '{"nw_src": "3.3.3.3/30", "nw_dst": "2.2.2.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000002
+curl -X POST -d '{"nw_src": "3.3.3.3/30", "nw_dst": "2.2.2.3/30", "nw_proto": "ICMP"}' http://localhost:8080/firewall/rules/0000000000000003
