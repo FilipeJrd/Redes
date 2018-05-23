aa7bba5947df5651de955be8042c3e1fae2f912c
Filipe Nogueira Jordão <filipejrd@hotmail.com>
2018-05-23 10:09:59 -0300

    Testing some stuff

--- ryu/ryu.py
+++ ryu/ryu.py
@@ -10,7 +10,6 @@ from ryu.lib.mac import haddr_to_str
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 
-
 class RyuFurioso(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_0.OFP_VERSION]
     _CONTEXTS = {'stplib': stplib.Stp}
@@ -21,44 +20,3 @@ class RyuFurioso(app_manager.RyuApp):
         self.stp = kwargs['stplib']
 
     
-    @set_ev_cls(stplib.EventPacketIn, MAIN_DISPATCHER)
-    def packet_in_handler(self, ev):
-        msg = ev.msg
-        datapath = msg.datapath
-        ofproto = datapath.ofproto
-        
-        pkt = packet.Packet(msg.data)
-        eth = pkt.get_protocol(ethernet.ethernet)
-
-        packet_dst = eth.dst #mac addr from dst
-        packet_src = eth.src #mac addr from src
-                    
-
-        dst, src, _eth_type = struct.unpack_from('!6s6sH', buffer(msg.data), 0)
-    
-        dpid = datapath.id
-        self.mac_to_port.setdefault(dpid, {})
-
-
-
-        self.logger.debug("packet in %s %s %s %s",
-                          dpid, haddr_to_str(src), haddr_to_str(dst),
-                          msg.in_port)
-
-        # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = msg.in_port
-
-        out_port = ofproto.OFPP_FLOOD
-
-
-        print("Packet Source: {}".format(packet_src))
-        print("Packet Destination: {}".format(packet_dst))
-        print("DataPath ID: {}".format(dpid))
-        
-        actions = [datapath.ofproto_parser.OFPActionOutput(out_port)]
-
-        # install a flow to avoid packet_in next time
-        out = datapath.ofproto_parser.OFPPacketOut(
-            datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
-            actions=actions)
-        datapath.send_msg(out)
