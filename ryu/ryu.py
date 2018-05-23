699e176193fb53af41b6f32abf08dac74a7d63b2
Filipe Nogueira Jordão <filipejrd@hotmail.com>
2018-05-23 10:14:04 -0300

    dasaasdsdadsa

--- ryu/ryu.py
+++ ryu/ryu.py
@@ -1,17 +1,5 @@
-import struct
-
 from ryu.base import app_manager
-from ryu.controller.handler import MAIN_DISPATCHER
-from ryu.controller.handler import set_ev_cls
-from ryu.ofproto import ofproto_v1_0
-from ryu.lib import dpid as dpid_lib
-from ryu.lib import stplib
-from ryu.lib.mac import haddr_to_str
-from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
 
-class RyuFurioso(app_manager.RyuApp):
+class L2Switch(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
-        super(RyuFurioso, self).__init__(*args, **kwargs)
-
-    
+        super(L2Switch, self).__init__(*args, **kwargs)
